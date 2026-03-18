f50a715b6ddd92decb722887bb6c347c8466c6b4
Yohanan Miteo <74419791+elYoha17@users.noreply.github.com>
2026-03-18 19:06:36 +0100

    style: reformat using pep8

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from constants import words, magnitude_words
 
 max_value = int("9" * 63)
 
+
 def main() -> None:
     is_running = True
-    
+
     while is_running:
         input_value = input("Entrez un nombre entier : ")
 
@@ -28,7 +29,7 @@ def main() -> None:
                 print("Ce nombre dépasse les limites autorisés.")
                 print()
                 continue
-                
+
             magnitudes = group_by_magnitude(input_number)
 
             result = ""
@@ -54,6 +55,7 @@ def main() -> None:
 
         print()
 
+
 def group_by_magnitude(num: int) -> list[str]:
     num_str = str(num)
     remainder = len(num_str) % 3
@@ -62,10 +64,11 @@ def group_by_magnitude(num: int) -> list[str]:
 
     return [num_str[i:i+3] for i in range(0, len(num_str), 3)][::-1]
 
+
 def convert_to_words(num: int) -> str:
     if not (0 <= num < 1000):
         raise Exception("Erreur : Le nombre doit être compris entre 0 et 999.")
-    
+
     if num in words:
         return words[num]
 
@@ -76,14 +79,14 @@ def convert_to_words(num: int) -> str:
         result += words[100]
     elif hundred > 1:
         result += words[hundred] + "-" + words[100]
-        
+
     if ten != 0 and ten * 10 + unit in words:
         result += ("" if result == "" else "-") + words[ten * 10 + unit]
         return result
-    
+
     if ten > 0:
         result += ("" if result == "" else "-") + words[ten * 10]
-        
+
     if unit == 1 and not (ten == 8 or ten == 0):
         result += "-et"
 
@@ -91,6 +94,7 @@ def convert_to_words(num: int) -> str:
         result += ("" if result == "" else "-") + words[unit]
 
     return result
-    
+
+
 if __name__ == "__main__":
     main()
